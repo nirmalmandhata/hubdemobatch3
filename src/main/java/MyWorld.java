7d64de208d5e6d1c93947ac9cbe38943680d7f91
Unknown <nirmalmandhata@gmail.com>
2018-07-10 16:08:44 +0530

    proj created

---
+++ src/main/java/MyWorld.java
@@ -0,0 +1,13 @@
+class MyWorld{
+	public static void main(String arg[]) {
+		String message = welcomeMessage();
+		System.out.println("\n\n"+message);
+		System.out.println("success----------- issue reporting");
+		System.out.println("success---------- issue 1 label ----- +demo @@@");
+
+	}
+	
+	public static String welcomeMessage() {
+		return "Wellcome to my world";
+	}
+}
